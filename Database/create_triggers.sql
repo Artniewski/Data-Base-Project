26b01ca7a6cf2c6c6a177d5688b88638ec9c6175
artniewski <artniewski@gmail.com>
2022-01-18 18:45:22 +0100

    Add Cars_in_stores triggers

---
+++ Database/create_triggers.sql
@@ -0,0 +1,14 @@
+USE `SpeedyGad`;
+
+delimiter //
+DROP TRIGGER IF EXISTS remove_cars_in_stroes_if_0_left;
+CREATE TRIGGER remove_cars_in_stroes_if_0_left
+    AFTER UPDATE
+    ON Cars_in_stores
+    FOR EACH ROW
+BEGIN
+    IF (NEW.quantity = 0) THEN
+        DELETE FROM Cars_in_stores WHERE quantity = 0;
+    END IF;
+END //
+delimiter ;
