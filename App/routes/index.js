88d04a850532949ba4cb81a81f409c01b57a3df0
artniewski <artniewski@gmail.com>
2022-01-19 03:19:29 +0100

    Add localStorage

--- App/routes/index.js
+++ App/routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+if (typeof localStorage === "undefined" || localStorage === null) {
+    const LocalStorage = require('node-localstorage').LocalStorage;
+    localStorage = new LocalStorage('./scratch');
+}
+
+router.use(function (req, res, next) {
+    //localStorage.setItem("accountType", "admin")
+    next();
+})
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    let type = localStorage.getItem("accountType")
+    switch (type) {
+        case "admin":
+            localStorage.setItem("accountType", "user")
+            res.render('index', {title: 'admin'});
+            break;
+        case "user":
+            localStorage.clear();
+            res.render('index', {title: 'user'})
+            break;
+        case "manager":
+            res.render('index', {title: 'manager'})
+            break;
+
+    }
+    res.render('index', {title: localStorage.getItem("counter")});
 });
 
 module.exports = router;
