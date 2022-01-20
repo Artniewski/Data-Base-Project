0af8e1c6d1d51775415b3f4a5787938de25c3c98
klwalter <kamil.walter4@gmail.com>
2022-01-20 14:20:33 +0100

    Add cancel_order procedure, delete add_car_to_store_if_order_status_is_cancelled trigger, add remove_model_if_no_cars_left trigger, fix add_car_model, fix add_order

--- Database/create_triggers.sql
+++ Database/create_triggers.sql
@@ -27,31 +27,45 @@ USE `SpeedyGad`;
 #     end;
 # $$
 -- TODO: zmiana nazwy trigger w sprawozdaniu remove_car_from_store_if_order_status_is_cancelled (remove -> add, from ->to)
+# DELIMITER $$
+# drop trigger if exists add_car_to_store_if_order_status_is_cancelled;
+# create trigger add_car_to_store_if_order_status_is_cancelled
+#     before update
+#     on orders
+#     for each row
+# begin
+#     declare brand_name varchar(50);
+#     declare brand_country varchar(50);
+#     declare car_name varchar(50);
+#     declare car_price float unsigned;
+#     declare car_max_speed decimal(5, 2) unsigned;
+#     if new.status = 'cancelled' then
+#         select name, country
+#         into brand_name, brand_country
+#         from Brands as b
+#         where b.ID = (select brandID
+#                       from Models as m
+#                       where new.modelID = m.ID);
+#         select name, price, max_speed
+#         into car_name,car_price,car_max_speed
+#         from Models
+#         where ID = NEW.modelID;
+#         call add_car_to_store(brand_name, brand_country, car_name, car_price, car_max_speed, new.storeID, 1, new.color);
+#
+#     end if;
+# end;
+# $$
+# DELIMITER ;
+
 DELIMITER $$
-drop trigger if exists add_car_to_store_if_order_status_is_cancelled;
-create trigger add_car_to_store_if_order_status_is_cancelled
-    after update
-    on orders
+drop trigger if exists remove_model_if_no_cars_left;
+create trigger remove_model_if_no_cars_left
+    after delete
+    on cars_in_stores
     for each row
 begin
-    declare brand_name varchar(50);
-    declare brand_country varchar(50);
-    declare car_name varchar(50);
-    declare car_price float unsigned;
-    declare car_max_speed decimal(5, 2) unsigned;
-    if new.status = 'cancelled' then
-        select name, country
-        into brand_name, brand_country
-        from Brands as b
-        where b.ID = (select brandID
-                      from Models as m
-                      where new.modelID = m.ID);
-        select name, price, max_speed
-        into car_name,car_price,car_max_speed
-        from Models
-        where ID = NEW.modelID;
-        call add_car_to_store(brand_name, brand_country, car_name, car_price, car_max_speed, new.storeID, 1, new.color);
-
+    if old.modelID not in (select modelID from cars_in_stores) then
+        delete from models where ID = old.modelID;
     end if;
 end;
 $$
